bf6f580614ab5df54e87c20cb4b69080f23c1ffe
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2020-02-22 14:50:30 +0300

    Added logging back

--- source/main.py
+++ source/main.py
@@ -1,8 +1,27 @@
+import logging
 import sys
 
 from PyQt5.QtWidgets import QApplication
 
+from modules._platform import get_platform
 from windows.main_window import BlenderLauncher
+
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler(stream=sys.stdout)
+logger.addHandler(handler)
+
+
+def handle_exception(exc_type, exc_value, exc_traceback):
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+
+    _format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(filename="BL.log", format=_format)
+    logger.error(get_platform(), exc_info=(exc_type, exc_value, exc_traceback))
+
+
+sys.excepthook = handle_exception
 
 
 def main():
