dfe6850e355ac46d68b9536dcc05263359438180
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2020-03-19 18:15:26 +0300

    Change settings window to main window

--- source/windows/settings_window.py
+++ source/windows/settings_window.py
@@ -1,16 +1,19 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QDialog, QFileDialog
+from PyQt5.QtWidgets import QDialog, QFileDialog, QMainWindow
 
 from modules.settings import *
-from ui.settings_window_design import Ui_Form
+from ui.settings_window_design import Ui_SettingsWindow
 from windows.base_window import BaseWindow
 
 
-class SettingsWindow(BaseWindow, Ui_Form):
+class SettingsWindow(QMainWindow, BaseWindow, Ui_SettingsWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.show()
+
+        self.CloseButton.setProperty("HeaderButton", True)
+        self.CloseButton.setProperty("CloseButton", True)
+        self.CloseButton.clicked.connect(self.close)
 
         self.LibraryFolderLineEdit.setText(str(get_library_folder()))
         self.LibraryFolderLineEdit.setCursorPosition(0)
@@ -24,6 +27,8 @@ class SettingsWindow(BaseWindow, Ui_Form):
             self.toggle_launch_when_system_starts)
         self.LaunchMinimizedToTrayCheckBox.clicked.connect(
             self.toggle_launch_minimized_to_tray)
+
+        self.show()
 
     def set_library_folder(self):
         library_folder = str(get_library_folder())
