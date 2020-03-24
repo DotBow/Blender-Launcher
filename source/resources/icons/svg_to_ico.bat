b53c9a7a713d6c952b0a28df6f61cb404e4d4cef
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2020-03-24 22:57:29 +0300

    Create svg_to_ico.bat

---
+++ source/resources/icons/svg_to_ico.bat
@@ -0,0 +1,3 @@
+start magick convert -background none -density 1024 bl_desktop.svg -define icon:auto-resize bl_desktop.ico
+start magick convert -background none -density 1024 bl_tray.svg -define icon:auto-resize bl_tray.ico
+pause
