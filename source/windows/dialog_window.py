2ca7676360d2ec8e6e4cd928e750d9be554a0047
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2020-04-22 21:56:32 +0300

    Refactor

--- source/windows/dialog_window.py
+++ source/windows/dialog_window.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import QMainWindow
 
@@ -6,31 +8,37 @@ from ui.dialog_window_design import Ui_DialogWindow
 from windows.base_window import BaseWindow
 
 
+class DialogIcon(Enum):
+    WARNING = 1
+    INFO = 2
+
+
 class DialogWindow(QMainWindow, BaseWindow, Ui_DialogWindow):
     accepted = pyqtSignal()
     cancelled = pyqtSignal()
 
-    def __init__(self, parent, text="Dialog Window", text1="OK", text2=None):
+    def __init__(self, parent, title="Warning", text="Dialog Window", accept_text="Accept", cancel_text="Cancel", icon=DialogIcon.WARNING):
         super().__init__()
         self.parent = parent
         self.setupUi(self)
 
-        self.setWindowTitle("Warning")
+        self.setWindowTitle(title)
         self.InfoLabel.setText(text)
         self.IconButton.setProperty("Icon", True)
+        self.AcceptButton.setText(accept_text)
+
+        if cancel_text is None:
+            self.CancelButton.hide()
+        else:
+            self.CancelButton.setText(cancel_text)
 
         if self.AcceptButton.sizeHint().width() > self.CancelButton.sizeHint().width():
             width = self.AcceptButton.sizeHint().width()
         else:
-            width = self.AcceptButton.sizeHint().width()
+            width = self.CancelButton.sizeHint().width()
 
         self.AcceptButton.setFixedWidth(width + 16)
         self.CancelButton.setFixedWidth(width + 16)
-
-        if text2 is None:
-            self.CancelButton.hide()
-
-        self.AcceptButton.setText(text1)
 
         self.AcceptButton.clicked.connect(self.accept)
         self.CancelButton.clicked.connect(self.cancel)
