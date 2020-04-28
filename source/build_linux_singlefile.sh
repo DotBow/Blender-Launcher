49f8b8d0192e5b3f672f2f898d3b4e6e30275ceb
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2020-04-28 22:24:12 +0300

    Create build_linux_singlefile.sh

---
+++ source/build_linux_singlefile.sh
@@ -0,0 +1,11 @@
+PYTHONOPTIMIZE=2 /home/oleg/.local/bin/pyinstaller \
+--clean \
+--noconsole \
+--noupx \
+--onefile \
+--windowed \
+--icon="resources\icons\desktop.ico" \
+--name="Blender Launcher" \
+--version-file="version.txt" \
+--add-binary "resources\icons\winblender.ico;files" \
+main.py
