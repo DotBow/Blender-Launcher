b586d29331af74360645a1f8bcb87750b3dc072c
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2020-05-17 23:05:08 +0300

    Update version to 1.0.0-rc.3

--- source/main.py
+++ source/main.py
@@ -26,7 +26,7 @@ sys.excepthook = handle_exception
 
 def main():
     app = QApplication(sys.argv)
-    app.setApplicationVersion("v1.0.0-rc.2")
+    app.setApplicationVersion("v1.0.0-rc.3")
     BlenderLauncher(app)
     app.exec_()
 
