95f6105c9d82ba4048fbcd6fab783c84538cc006
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2020-05-28 23:37:55 +0300

    Settings window refactor

--- source/windows/settings_window.py
+++ source/windows/settings_window.py
@@ -1,5 +1,9 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QDialog, QFileDialog, QMainWindow, QCheckBox
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import (QCheckBox, QDialog, QFileDialog, QHBoxLayout,
+                             QLabel, QLineEdit, QMainWindow, QPushButton,
+                             QVBoxLayout)
 
 from modules.settings import *
 from ui.settings_window_design import Ui_SettingsWindow
@@ -14,25 +18,47 @@ class SettingsWindow(QMainWindow, BaseWindow, Ui_SettingsWindow):
 
         self.setWindowTitle("Settings")
 
+        self.HeaderLayout = QHBoxLayout()
+        self.HeaderLayout.setContentsMargins(36, 1, 1, 0)
+        self.HeaderLayout.setSpacing(0)
+        self.CentralLayout.addLayout(self.HeaderLayout)
+
+        self.CloseButton = \
+            QPushButton(QIcon(":resources/icons/close.svg"), "")
+        self.CloseButton.setIconSize(QSize(20, 20))
+        self.CloseButton.setFixedSize(36, 32)
         self.CloseButton.setProperty("HeaderButton", True)
         self.CloseButton.setProperty("CloseButton", True)
         self.CloseButton.clicked.connect(self.close)
+        self.HeaderLabel = QLabel("Settings")
+        self.HeaderLabel.setAlignment(Qt.AlignCenter)
 
+        self.HeaderLayout.addWidget(self.HeaderLabel, 1)
+        self.HeaderLayout.addWidget(self.CloseButton, 0, Qt.AlignRight)
+
+        self.LibraryFolderLineEdit = QLineEdit()
         self.LibraryFolderLineEdit.setText(str(get_library_folder()))
+        self.LibraryFolderLineEdit.setContextMenuPolicy(Qt.NoContextMenu)
+        self.LibraryFolderLineEdit.setReadOnly(True)
         self.LibraryFolderLineEdit.setCursorPosition(0)
+ 
+        self.SetLibraryFolderButton = \
+            QPushButton(QIcon(":resources/icons/folder.svg"), "")
         self.SetLibraryFolderButton.clicked.connect(self.set_library_folder)
 
+        self.LaunchWhenSystemStartsCheckBox = QCheckBox(
+            "Launch When System Starts")
         self.LaunchWhenSystemStartsCheckBox.setChecked(
             get_launch_when_system_starts())
-        self.LaunchMinimizedToTrayCheckBox.setChecked(
-            get_launch_minimized_to_tray())
         self.LaunchWhenSystemStartsCheckBox.clicked.connect(
             self.toggle_launch_when_system_starts)
+
+        self.LaunchMinimizedToTrayCheckBox = QCheckBox(
+            "Launch Minimized To Tray")
+        self.LaunchMinimizedToTrayCheckBox.setChecked(
+            get_launch_minimized_to_tray())
         self.LaunchMinimizedToTrayCheckBox.clicked.connect(
             self.toggle_launch_minimized_to_tray)
-
-        if get_platform() == 'Linux':
-            self.LaunchWhenSystemStartsCheckBox.hide()
 
         self.EnableHighDpiScalingCheckBox = \
             QCheckBox("Enable High DPI Scaling")
@@ -40,7 +66,28 @@ class SettingsWindow(QMainWindow, BaseWindow, Ui_SettingsWindow):
             self.toggle_enable_high_dpi_scaling)
         self.EnableHighDpiScalingCheckBox.setChecked(
             get_enable_high_dpi_scaling())
-        self.verticalLayout_2.addWidget(self.EnableHighDpiScalingCheckBox)
+
+        self.SettingsLayout = QVBoxLayout()
+        self.SettingsLayout.setContentsMargins(6, 6, 6, 6)
+        self.SettingsLayout.setSpacing(6)
+        self.CentralLayout.addLayout(self.SettingsLayout)
+
+        self.LibraryFolderLayout = QHBoxLayout()
+        self.LibraryFolderLayout.setContentsMargins(1, 1, 1, 1)
+        self.LibraryFolderLayout.setSpacing(0)
+
+        self.SettingsLayout.addWidget(QLabel("Library Folder:"))
+        self.SettingsLayout.addLayout(self.LibraryFolderLayout)
+        self.LibraryFolderLayout.addWidget(self.LibraryFolderLineEdit)
+        self.LibraryFolderLayout.addWidget(self.SetLibraryFolderButton)
+
+        self.SettingsLayout.addWidget(QLabel("System:"))
+
+        if get_platform() == 'Windows':
+            self.SettingsLayout.addWidget(self.LaunchWhenSystemStartsCheckBox)
+
+        self.SettingsLayout.addWidget(self.LaunchMinimizedToTrayCheckBox)
+        self.SettingsLayout.addWidget(self.EnableHighDpiScalingCheckBox)
 
         self.show()
 
