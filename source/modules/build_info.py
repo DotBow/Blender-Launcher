f424e2559184c3094cb9f3487368a398cb2e4355
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2020-09-06 00:24:18 +0300

    Use the same extension remove method for all platforms

--- source/modules/build_info.py
+++ source/modules/build_info.py
@@ -37,12 +37,7 @@ class BuildInfo:
 
     def get_name(self):
         if self.link_type == 'link':
-            platform = get_platform()
-
-            if platform == 'Linux':
-                return Path(self.link).with_suffix('').stem
-            elif platform == 'Windows':
-                return Path(self.link).stem
+            return Path(self.link).with_suffix('').stem
         elif self.link_type == 'path':
             return Path(self.link).name
 
