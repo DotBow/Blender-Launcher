c709c8797ee485c907b466abb7f9bf7d20436c2d
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2020-09-09 20:28:29 +0300

    Delete itself at the end

--- update.bat
+++ update.bat
@@ -1,1 +1,1 @@
-XCOPY /Y "%TEMP%\Blender Launcher.exe" "Blender Launcher.exe" & START "" "Blender Launcher.exe"
+XCOPY /Y "%TEMP%\Blender Launcher.exe" "Blender Launcher.exe" & START "" "Blender Launcher.exe" & (GOTO) 2>NUL & DEL "%~f0"
