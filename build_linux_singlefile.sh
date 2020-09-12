e194ea53131b9afb938f89eb3fb4fc0a47f27701
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2020-09-12 22:47:23 +0300

    Add update.sh as binary

--- build_linux_singlefile.sh
+++ build_linux_singlefile.sh
@@ -7,4 +7,5 @@ PYTHONOPTIMIZE=2 pyinstaller \
 --icon="source/resources/icons/desktop.ico" \
 --name="Blender Launcher" \
 --version-file="source/version.txt" \
+--add-binary="update.sh;files" \
 source/main.py
