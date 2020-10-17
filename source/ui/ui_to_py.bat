b57f19e475b371f918ccf066c039becd9be267b2
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2020-10-17 17:13:02 +0300

    Remove files related to Designer

--- source/ui/ui_to_py.bat
+++
@@ -1,4 +0,0 @@
-pyuic5 main_window_design.ui -o main_window_design.py
-pyuic5 settings_window_design.ui -o settings_window_design.py
-pyuic5 dialog_window_design.ui -o dialog_window_design.py
-pause
