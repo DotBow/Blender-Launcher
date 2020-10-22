c886784001101ff8e31197782e14adeb6a6ac5d6
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2020-10-22 20:16:47 +0300

    Add LD_LIBRARY_PATH setup for GNU/Linux and *BSD

--- source/modules/_platform.py
+++ source/modules/_platform.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from locale import LC_ALL, setlocale
 
@@ -24,3 +25,21 @@ def set_locale():
         setlocale(LC_ALL, 'eng_usa')
     elif platform == 'Linux':
         setlocale(LC_ALL, 'en_US.UTF-8')
+
+
+def get_environment():
+    # Make a copy of the environment
+    env = dict(os.environ)
+    # For GNU/Linux and *BSD
+    lp_key = 'LD_LIBRARY_PATH'
+    lp_orig = env.get(lp_key + '_ORIG')
+
+    if lp_orig is not None:
+        # Restore the original, unmodified value
+        env[lp_key] = lp_orig
+    else:
+        # This happens when LD_LIBRARY_PATH was not set
+        # Remove the env var as a last resort
+        env.pop(lp_key, None)
+
+    return env
