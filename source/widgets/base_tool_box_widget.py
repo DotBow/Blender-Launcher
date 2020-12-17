dc68c14176feeba96a1456268e6477717d4cc782
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2020-12-17 22:15:14 +0300

    Use tab widget instead of tool box

--- source/widgets/base_tool_box_widget.py
+++ source/widgets/base_tool_box_widget.py
@@ -1,8 +1,7 @@
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QToolBox
+from PyQt5.QtWidgets import QTabWidget
 
 
-class BaseToolBoxWidget(QToolBox):
+class BaseToolBoxWidget(QTabWidget):
     def __init__(self, parent=None):
         super().__init__()
         self.pages = []
@@ -10,22 +9,10 @@ class BaseToolBoxWidget(QToolBox):
         self.list_widgets = set()
 
         self.setContentsMargins(0, 0, 0, 0)
-        self.layout().setSpacing(0)
-        self.currentChanged.connect(self.current_changed)
+        self.setTabPosition(QTabWidget.West)
 
     def add_page_widget(self, page_widget, page_name):
         self.pages.append(page_widget)
-        self.addItem(page_widget, QIcon(
-            ":resources/icons/page_closed.svg"), page_name)
+        self.addTab(page_widget, page_name)
         self.list_widgets.add(page_widget.list_widget)
         return page_widget.list_widget
-
-    def current_changed(self, index):
-        icon_page_opened = QIcon(":resources/icons/page_opened.svg")
-        icon_page_closed = QIcon(":resources/icons/page_closed.svg")
-
-        for i in range(self.count()):
-            if i != index:
-                self.setItemIcon(i, icon_page_closed)
-
-        self.setItemIcon(index, icon_page_opened)
