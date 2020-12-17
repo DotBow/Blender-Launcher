9ce9be08e1a9b4c6e860a8bf031e8f4f5852ae3e
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2020-12-18 00:30:17 +0300

    Add custom show function

--- source/widgets/base_menu_widget.py
+++ source/widgets/base_menu_widget.py
@@ -1,4 +1,5 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtGui import QCursor
 from PyQt5.QtWidgets import QMenu
 
 
@@ -6,3 +7,18 @@ class BaseMenuWidget(QMenu):
     def __init__(self):
         super().__init__()
         self.setWindowFlags(self.windowFlags() | Qt.NoDropShadowWindowHint)
+
+    def _show(self):
+        cursor = QCursor.pos()
+        cursor = cursor - QPoint(15, 15)
+        i = 0
+
+        for action in self.actions():
+            if action.isVisible() and action.isEnabled():
+                self.setActiveAction(action)
+                cursor.setY(cursor.y() - i * 30)
+                break
+
+            i = i + 1
+
+        self.exec_(cursor)
