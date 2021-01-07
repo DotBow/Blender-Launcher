91c7628687003384ae80775e10641f7bd7e3a69d
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2021-01-07 19:45:12 +0300

    Add button preset with left aligned icon

---
+++ source/widgets/left_icon_button_widget.py
@@ -0,0 +1,16 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QHBoxLayout, QLabel, QPushButton
+
+
+class LeftIconButtonWidget(QPushButton):
+    def __init__(self, text, icon):
+        super(LeftIconButtonWidget, self).__init__(None)
+        self.setIcon(icon)
+        self.setStyleSheet("text-align:left;")
+        self.setLayout(QHBoxLayout())
+
+        label = QLabel(text)
+        label.setAlignment(Qt.AlignCenter)
+        label.setAttribute(Qt.WA_TransparentForMouseEvents, True)
+
+        self.layout().addWidget(label)
