30d5210942eae45b0c5fd0f7a09fba7dd1f312cc
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2021-01-13 20:21:50 +0300

    Make settings window wider

--- source/ui/settings_window_ui.py
+++ source/ui/settings_window_ui.py
@@ -6,7 +6,7 @@ class Ui_SettingsWindow(object):
     def setupUi(self, SettingsWindow):
         SettingsWindow.setObjectName("SettingsWindow")
         SettingsWindow.setWindowModality(QtCore.Qt.ApplicationModal)
-        SettingsWindow.resize(400, 100)
+        SettingsWindow.resize(480, 100)
         sizePolicy = QtWidgets.QSizePolicy(
             QtWidgets.QSizePolicy.Preferred,
             QtWidgets.QSizePolicy.MinimumExpanding)
@@ -15,7 +15,7 @@ class Ui_SettingsWindow(object):
         sizePolicy.setHeightForWidth(
             SettingsWindow.sizePolicy().hasHeightForWidth())
         SettingsWindow.setSizePolicy(sizePolicy)
-        SettingsWindow.setMinimumSize(QtCore.QSize(400, 100))
+        SettingsWindow.setMinimumSize(QtCore.QSize(480, 100))
         self.CentralWidget = QtWidgets.QWidget(SettingsWindow)
         self.CentralWidget.setObjectName("CentralWidget")
         self.CentralLayout = QtWidgets.QVBoxLayout(self.CentralWidget)
