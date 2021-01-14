43b76489a8c2d3663ca48c786efcc4edb804c891
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2021-01-14 11:43:14 +0300

    Add elided text label for proper scaling of list contents

---
+++ source/widgets/elided_text_label.py
@@ -0,0 +1,18 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFontMetrics
+from PyQt5.QtWidgets import QLabel
+
+
+class ElidedTextLabel(QLabel):
+    def __init__(self, text):
+        super(ElidedTextLabel, self).__init__(None)
+        self.text = text
+
+    def setElidedText(self):
+        metrics = QFontMetrics(self.font())
+        width = self.width() - 2
+        clippedText = metrics.elidedText(self.text, Qt.ElideRight, width)
+        self.setText(clippedText)
+
+    def resizeEvent(self, event):
+        self.setElidedText()
