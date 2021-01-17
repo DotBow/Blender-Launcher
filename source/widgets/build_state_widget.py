e8df3e12fda12c1e116c5f3ee7f492e571fe437e
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2021-01-17 11:08:44 +0300

    Simplify code, unify icon button creation

--- source/widgets/build_state_widget.py
+++ source/widgets/build_state_widget.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QHBoxLayout, QPushButton
+from PyQt5.QtWidgets import QHBoxLayout, QPushButton, QWidget
 
 
 class BuildStateWidget(QWidget):
@@ -10,27 +10,28 @@ class BuildStateWidget(QWidget):
         self.layout.setContentsMargins(0, 0, 4, 0)
         self.layout.setSpacing(0)
 
-        self.widgetFake = QPushButton()
-        self.widgetFake.setProperty("Icon", True)
-        self.widgetFake.setFixedSize(24, 24)
-        self.widgetFake.setEnabled(False)
+        self.widgetFake = self.IconButton()
 
-        self.countButton = QPushButton("0")
-        self.countButton.setProperty("Count", True)
-        self.countButton.setFixedSize(24, 24)
-        self.countButton.setEnabled(False)
+        self.countButton = self.IconButton(text="0", prop="Count")
         self.countButton.hide()
 
-        self.widgetFavorite = QPushButton()
-        self.widgetFavorite.setProperty("Icon", True)
-        self.widgetFavorite.setFixedSize(24, 24)
-        self.widgetFavorite.setEnabled(False)
-        self.widgetFavorite.setIcon(self.parent.icon_fake)
+        self.widgetFavorite = self.IconButton(self.parent.icon_fake)
         self.widgetFavorite.hide()
 
         self.layout.addWidget(self.widgetFake)
         self.layout.addWidget(self.countButton)
         self.layout.addWidget(self.widgetFavorite)
+
+    def IconButton(self, icon=None, text="", prop="Icon"):
+        button = QPushButton(text)
+
+        if icon is not None:
+            button.setIcon(icon)
+
+        button.setProperty(prop, True)
+        button.setFixedSize(24, 24)
+        button.setEnabled(False)
+        return button
 
     def setCount(self, count):
         if count > 0:
