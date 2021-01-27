f0549d9c25a960d7495ce5344fb6afa9d5f91e40
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2021-01-27 14:56:22 +0300

    Add class for managing row content

--- source/layouts/settings_form_layout.py
+++ source/layouts/settings_form_layout.py
@@ -1,6 +1,17 @@
 from PyQt5.QtWidgets import QFormLayout, QLabel
 
 
+class SettingsFormLayoutRow():
+    def __init__(self, label, widget):
+        super().__init__()
+        self.label = label
+        self.widget = widget
+
+    def setEnabled(self, enabled=True):
+        self.label.setEnabled(enabled)
+        self.widget.setEnabled(enabled)
+
+
 class SettingsFormLayout(QFormLayout):
     def __init__(self, label_width):
         super().__init__()
@@ -13,3 +24,5 @@ class SettingsFormLayout(QFormLayout):
         label = QLabel(label_text)
         label.setFixedWidth(self.label_width)
         self.addRow(label, widget)
+
+        return SettingsFormLayoutRow(label, widget)
