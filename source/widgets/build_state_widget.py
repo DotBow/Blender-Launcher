888dc40f5dd37f661b1a173b14c9bddc95c59116
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2021-02-22 20:42:35 +0300

    Add animated download and extract states

--- source/widgets/build_state_widget.py
+++ source/widgets/build_state_widget.py
@@ -1,3 +1,4 @@
+from PyQt5.QtCore import QEasingCurve, QPropertyAnimation, QRect, QSize
 from PyQt5.QtWidgets import QHBoxLayout, QPushButton, QWidget
 
 
@@ -18,17 +19,27 @@ class BuildStateWidget(QWidget):
         self.newBuildIcon = self.IconButton(self.parent.filled_circle)
         self.newBuildIcon.hide()
 
+        self.downloadIcon = self.IconButton(self.parent.icon_download)
+        self.downloadIcon.hide()
+
+        self.extractIcon = self.IconButton(self.parent.icon_file)
+        self.extractIcon.hide()
+
         self.layout.addWidget(self.fakeIcon)
         self.layout.addWidget(self.countIcon)
         self.layout.addWidget(self.newBuildIcon)
+        self.layout.addWidget(self.downloadIcon)
+        self.layout.addWidget(self.extractIcon)
 
         self.active_icon = self.fakeIcon
+        self.setDownload()
 
     def IconButton(self, icon=None, text="", prop="Icon"):
         button = QPushButton(text)
 
         if icon is not None:
             button.setIcon(icon)
+            button.setIconSize(QSize(24, 24))
 
         button.setProperty(prop, True)
         button.setFixedSize(24, 24)
@@ -55,3 +66,61 @@ class BuildStateWidget(QWidget):
             self.active_icon.hide()
             self.fakeIcon.show()
             self.active_icon = self.fakeIcon
+
+    def setDownload(self, show=True):
+        if show:
+            self.active_icon.hide()
+            self.downloadIcon.show()
+            self.active_icon = self.downloadIcon
+            self.download_anim()
+        else:
+            self.active_icon.hide()
+            self.fakeIcon.show()
+            self.active_icon = self.fakeIcon
+            self.anim.deleteLater()
+
+    def setExtract(self, show=True):
+        if show:
+            self.active_icon.hide()
+            self.extractIcon.show()
+            self.active_icon = self.extractIcon
+            self.extract_anim()
+        else:
+            self.active_icon.hide()
+            self.fakeIcon.show()
+            self.active_icon = self.fakeIcon
+            self.anim.deleteLater()
+
+    def download_anim(self):
+        self.anim = QPropertyAnimation(self.downloadIcon, b"geometry")
+        self.anim.setDuration(1000)
+        self.anim.setLoopCount(-1)
+        geometry = self.downloadIcon.geometry()
+        self.anim.setStartValue(QRect(
+            geometry.x(), geometry.y() - geometry.height(),
+            geometry.width(), geometry.height()))
+        self.anim.setEndValue(QRect(
+            geometry.x(), geometry.height() * 1.25,
+            geometry.width(), geometry.height()))
+        self.anim.start()
+
+    def extract_anim(self):
+        self.anim = QPropertyAnimation(self.extractIcon, b"geometry")
+        self.anim.setDuration(2000)
+        self.anim.setLoopCount(-1)
+        self.anim.setEasingCurve(QEasingCurve.OutCubic)
+        geometry = self.extractIcon.geometry()
+        self.anim.setStartValue(QRect(
+            geometry.x(), geometry.height() * 1.25,
+            geometry.width(), geometry.height()))
+        self.anim.setKeyValueAt(0.3, QRect(
+            geometry.x(), geometry.y() + geometry.height() * 0.25,
+            geometry.width(), geometry.height()))
+        self.anim.setKeyValueAt(0.7, QRect(
+            geometry.x(), geometry.y() + geometry.height() * 0.25,
+            geometry.width(), geometry.height()))
+        self.anim.setEndValue(QRect(
+            geometry.x() + geometry.width() * 1.25,
+            geometry.y() + geometry.height() * 0.25,
+            geometry.width(), geometry.height()))
+        self.anim.start()
