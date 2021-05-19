002a72cd383fcd9b16a434c56b5aedea9e12c0a6
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2021-05-20 00:09:29 +0300

    Update version to 1.12.1

--- source/main.py
+++ source/main.py
@@ -9,7 +9,7 @@ from modules._platform import get_platform
 from windows.main_window import BlenderLauncher
 from windows.update_window import BlenderLauncherUpdater
 
-version = "1.12.0"
+version = "1.12.1"
 logger = logging.getLogger(__name__)
 handler = logging.StreamHandler(stream=sys.stdout)
 logger.addHandler(handler)
