4c022f887d5128a6632dfa18380f1cbbcccaab65
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2021-08-13 11:28:03 +0300

    Remove subversion indent calculation and make it constant

--- source/widgets/library_widget.py
+++ source/widgets/library_widget.py
@@ -505,9 +505,6 @@ class LibraryWidget(BaseBuildWidget):
             self.build_info.is_favorite = True
             self.write_build_info()
 
-        # It fixes the issue with indenting last added widget
-        self.parent.UserFavoritesListWidget.resize()
-
     @QtCore.pyqtSlot()
     def remove_from_favorites(self):
         if self.parent_widget is None:
