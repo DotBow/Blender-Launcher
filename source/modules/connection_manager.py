f33841c3be4355b0132b0672efd7619e6591e3ef
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2022-01-25 00:02:36 +0300

    Add comments

--- source/modules/connection_manager.py
+++ source/modules/connection_manager.py
@@ -34,22 +34,25 @@ class ConnectionManager():
                 get_cwd() / "source/resources/certificates/custom.pem").as_posix()
 
     def setup(self):
-        if self.proxy_type == 0:  # None
+        if self.proxy_type == 0:  # Use generic requests
             if get_use_custom_tls_certificates():
+                # Generic requests with CERT_REQUIRED
                 self.manager = PoolManager(
                     num_pools=50, maxsize=10, headers=self._headers,
                     cert_reqs=ssl.CERT_REQUIRED,
                     ca_certs=self.cacert)
             else:
+                # Generic requests w/o CERT_REQUIRED
                 self.manager = PoolManager(
                     num_pools=50, maxsize=10, headers=self._headers)
-        else:  # Use proxy
+        else:  # Use Proxy
             ip = get_proxy_host()
             port = get_proxy_port()
             scheme = proxy_types_chemes[self.proxy_type]
 
-            if self.proxy_type > 2:
+            if self.proxy_type > 2:  # Use SOCKS Proxy
                 if get_use_custom_tls_certificates():
+                    # SOCKS Proxy with CERT_REQUIRED
                     self.manager = SOCKSProxyManager(
                         proxy_url="{0}{1}:{2}".format(scheme, ip, port),
                         num_pools=50, maxsize=10, headers=self._headers,
@@ -57,23 +60,27 @@ class ConnectionManager():
                         password=get_proxy_password(),
                         cert_reqs=ssl.CERT_REQUIRED, ca_certs=self.cacert)
                 else:
+                    # SOCKS Proxy w/o CERT_REQUIRED
                     self.manager = SOCKSProxyManager(
                         proxy_url="{0}{1}:{2}".format(scheme, ip, port),
                         num_pools=50, maxsize=10, headers=self._headers,
                         username=get_proxy_user(),
                         password=get_proxy_password())
-            else:
+            else:  # Use HTTP Proxy
+                # HTTP Proxy autherification headers
                 auth_headers = make_headers(
                     proxy_basic_auth='{0}:{1}'.format(
                         get_proxy_user(), get_proxy_password()))
 
                 if get_use_custom_tls_certificates():
+                    # HTTP Proxy with CERT_REQUIRED
                     self.manager = ProxyManager(
                         proxy_url="{0}{1}:{2}".format(scheme, ip, port),
                         num_pools=50, maxsize=10,
                         headers=self._headers, proxy_headers=auth_headers,
                         cert_reqs=ssl.CERT_REQUIRED, ca_certs=self.cacert)
                 else:
+                    # HTTP Proxy w/o CERT_REQUIRED
                     self.manager = ProxyManager(
                         proxy_url="{0}{1}:{2}".format(scheme, ip, port),
                         num_pools=50, maxsize=10,
