aa5061bf23812362b57d61f126762b5cced27336
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2022-01-30 13:46:07 +0300

    Delete no longer needed OS dependent scripts

--- build_rc.sh
+++
@@ -1,1 +0,0 @@
-pyrcc5 source/resources/resources.qrc -o source/resources_rc.py
--- build_style.bat
+++
@@ -1,3 +0,0 @@
-cd /d "%~dp0source\resources\styles"
-if exist "global.qss" del /Q "global.qss"
-type *.css > global.qss
