dc756d8086f4049635273dd0c69175d7485e34e8
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2022-04-14 17:22:56 +0300

    Remove colors conversion

--- source/resources/icons/bl/make_ico.bat
+++ source/resources/icons/bl/make_ico.bat
@@ -1,2 +1,2 @@
-start magick convert -background none bl_16.png bl_24.png bl_32.png bl_48.png bl_64.png bl_72.png bl_96.png bl_128.png bl_180.png bl_256.png -colors 256 bl.ico
+start magick convert -background none bl_16.png bl_24.png bl_32.png bl_48.png bl_64.png bl_72.png bl_96.png bl_128.png bl_180.png bl_256.png bl.ico
 pause
