f39e0634d3777cbd908787f31e1767b7a50ee5ba
Oleg Stepanov <stepanovoleg.dev@gmail.com>
2022-04-27 16:11:02 +0300

    Add XLib for linux to support Fedora

--- build_linux.sh
+++ build_linux.sh
@@ -1,6 +1,7 @@
 PYTHONOPTIMIZE=2 pyinstaller \
 --hidden-import "pynput.keyboard._xorg" \
 --hidden-import "pynput.mouse._xorg" \
+--hidden-import "python-xlib" \
 --clean \
 --noconsole \
 --noupx \
