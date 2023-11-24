44e8458f860348a8d4f549efdb3b63620bfeb3bb
Victor C <victor.ched@gmail.com>
2023-11-23 21:36:44 -0500

    Change Update Git URL

--- source/threads/scraper.py
+++ source/threads/scraper.py
@@ -47,7 +47,7 @@ class Scraper(QThread):
 
     def get_latest_tag(self):
         r = self.manager._request(
-            'GET', 'https://github.com/DotBow/Blender-Launcher/releases/latest')
+            'GET', 'https://github.com/Victor-IX/Blender-Launcher/releases/latest')
 
         if r is None:
             return
